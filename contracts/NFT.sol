77a58001fbe96980ae628a3b8d24134cee66faa4
wtc <londiwemabuza94@gmail.com>
2026-04-11 12:54:50 +0200

    finished contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -8,16 +8,21 @@ contract NFT is ERC1155, Ownable {
     uint256 public nextTokenId;
     string public baseURI;
 
-    constructor(string memory _baseURI) ERC721("ABC", "ABC") {
+    string public name;
+    string public symbol;
+
+    mapping(uint256 => uint256) public totalSupply;
+
+    constructor(string memory _baseURI) ERC1155(_baseURI) {
         baseURI = _baseURI;
+        name = "ABC";
+        symbol = "ABC";
     }
 
-    function _baseURI() internal view override returns (string memory) {
-        return baseURI;
-    }
 
     function mint(address to) public onlyOwner {
-        _safeMint(to, nextTokenId);
+        _mint(to, nextTokenId, 1, "");
+        totalSupply[nextTokenId]++;
         nextTokenId++;
     }
 
@@ -25,11 +30,11 @@ contract NFT is ERC1155, Ownable {
         baseURI = _baseURI;
     }
 
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amount) public onlyOnwer{
-        _mintBatch(to, id, amounts, "");
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amount) public onlyOwner{
+        _mintBatch(to, ids, amount, "");
 
         for (uint i = 0; i< ids.length; i++){
-            totalSupply[ids[i]] += amounts[i];
+            totalSupply[ids[i]] += amount[i];
         }
     }
 } 
---
+++ scripts/deploy.js
@@ -0,0 +1,7 @@
+const {ethers} = require("hardhat");
+
+async function main(){
+    const [deployer, user1] = await ethers.getSigners();
+
+}
+main().catch(console.error);
